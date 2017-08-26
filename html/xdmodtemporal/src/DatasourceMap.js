8591cd914f44270761d725cc93800e7b3b833d5c
singhgag <singhgag@indiana.edu>
2017-08-26 02:30:13 -0400

    Major updates on Sankey and some design fixes on ForceNetwork

--- html/xdmodtemporal/src/DatasourceMap.js
+++ html/xdmodtemporal/src/DatasourceMap.js
@@ -2,7 +2,22 @@
  * @description Maps pretty names for datasource URLs. Mapped from (ng-data-field) 
  * @type {Object} */
  var globalDatasourceMap = {
-   
+    one: {
+        url: '../../data/sampleData.json',
+        type: 'static'
+    },
+    sampleGeoData: {
+        url: '../../data/sampleGeoData.json',
+        type: 'static'
+    },
+    apiTest: {
+        url: 'http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&format=json',
+        type: 'api',
+        params: {
+            api_key: '',
+            artist: 'QUERYSTRING'
+        }
+    },
     copiData: {
         url: 'data/copiData.json'
     },
@@ -10,7 +25,7 @@
         url: 'data/copiDataAsRecords.json'
     },
     sankeyData: {
-        url: 'data/sankey2_latest.json'
+        url: 'data/sankey3.json'
     }
 
 }
